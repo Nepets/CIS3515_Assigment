c03c34189d2aaaed51a8330bd5b56e918c0b939f
Steven <Stependang@gmail.com>
2021-09-27 21:18:03 -0400

    Added images

--- app/src/main/java/edu/temple/cis3515_assigment_3/MainActivity.kt
+++ app/src/main/java/edu/temple/cis3515_assigment_3/MainActivity.kt
@@ -3,7 +3,7 @@ package edu.temple.cis3515_assigment_3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ImageActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
