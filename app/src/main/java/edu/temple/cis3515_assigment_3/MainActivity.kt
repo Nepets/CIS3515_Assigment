b02fcc4b5ff11352f98885b242f6f6ce95393cca
Steven <Stependang@gmail.com>
2021-09-27 21:32:08 -0400

    Creating ImageAdapter

--- app/src/main/java/edu/temple/cis3515_assigment_3/MainActivity.kt
+++ app/src/main/java/edu/temple/cis3515_assigment_3/MainActivity.kt
@@ -2,10 +2,23 @@ package edu.temple.cis3515_assigment_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    private fun getData(): Array<ImageObject> {
+        return arrayOf(ImageObject("Black Clover", R.drawable.blackclover), ImageObject("Chainsaw Man",R.drawable.chainsawman),
+            ImageObject("Demon Slayer", R.drawable.demonslayer), ImageObject("Dr. Stone",R.drawable.drstone),
+            ImageObject("Dragon Ball Z",R.drawable.dragonball), ImageObject("Haikyuu",R.drawable.haikyuu),
+            ImageObject("One Piece",R.drawable.juju), ImageObject("One Punch",R.drawable.onepunch), ImageObject("Seraph no End", R.drawable.seraphend))
+
+    }
+
 }
