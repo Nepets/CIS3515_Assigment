3b57b574726539084e877e556ff2878b05b604ff
Steven <Stependang@gmail.com>
2021-09-27 21:02:46 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/cis3515_assigment_3/ImageActivity.kt
@@ -0,0 +1,11 @@
+package edu.temple.cis3515_assigment_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
